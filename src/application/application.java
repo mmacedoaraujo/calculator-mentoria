e8405868310bcc877044caf91c7b5874fcca062d
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-10-17 14:05:36 -0300

    simplified application class

--- src/application/application.java
+++ src/application/application.java
@@ -10,16 +10,8 @@ public class application {
     public static void main(String[] args) {
         Menus menus = new Menus();
 
-        String main = menus.MainMenu();
-        if (main.contains("1")) {
-            menus.regularCalculatorMenu();
-        }
-        if (main.contains("2")) {
-            menus.BMICalculatorMenu();
-        }
-        if (main.contains("3")) {
-            menus.AreaCalculatorMenu();
-        }
+        menus.mainMenu();
+
 
 
 
--- src/model/entities/Menus.java
+++ src/model/entities/Menus.java
@@ -1,35 +1,87 @@
 package model.entities;
 
+import com.sun.tools.javac.Main;
 import utils.Utils;
 
 import java.util.Scanner;
 
 public class Menus {
 
-    private String answer;
     private RegularCalculator regularCalculator;
     Scanner input = new Scanner(System.in);
 
     private Utils utils = new Utils();
 
-    public String MainMenu() {
-
+    public void mainMenu() {
+        String mainMenuAnswer = "";
         System.out.println("Bem vindo à minha calculadora multi uso!\nPor favor escolha uma das opções abaixo:");
         System.out.println("1. Calculadora Padrão");
         System.out.println("2. Calculadora de IMC");
         System.out.println("3. Calculadora de Área");
         System.out.print("Digite sua escolha: ");
-        answer = input.nextLine();
+        mainMenuAnswer = input.nextLine();
 
-        while (utils.regexMatcher("[4-9]+|[\\d]{2,}", answer)) {
+        while (utils.regexMatcher("[4-9]+|[\\d]{2,}", mainMenuAnswer)) {
             System.out.print("Opção inválida, tente novamente: ");
-            answer = input.nextLine();
+            mainMenuAnswer = input.nextLine();
         }
-        return answer;
+
+        if (mainMenuAnswer.contains("1")) {
+            regularCalculatorMenu();
+        }
+        if (mainMenuAnswer.contains("2")) {
+            BMICalculatorMenu();
+        }
+        if (mainMenuAnswer.contains("3")) {
+            AreaCalculatorMenu();
+        }
     }
 
     public void regularCalculatorMenu() {
-        System.out.println("Calculadora Padrão");
+        String answer = "";
+        System.out.println("Você escolheu: Calculadora Padrão\n" +
+                "Agora escolha qual operação deseja realizar:");
+        System.out.println("1. Soma");
+        System.out.println("2. Subtração");
+        System.out.println("3. Divisão");
+        System.out.println("4. Multiplicação");
+        System.out.println("5. Potenciação");
+        System.out.println("6. Retornar ao menu principal");
+
+        System.out.print("Digite sua escolha: ");
+        answer = input.nextLine();
+
+        while (utils.regexMatcher("[7-9]+|[\\d]{2,}", answer)) {
+            System.out.print("Opção inválida, tente novamente: ");
+            answer = input.nextLine();
+        }
+
+        if (answer.contains("1")) {
+            System.out.println("Você escolheu a operação 1: Soma" +
+                    "\nDeseja seguir em frente? [y/n]");
+            String answerSubMenu = input.nextLine();
+
+            System.out.print("Digite o primeiro valor a ser somado: ");
+            double firstNumber = input.nextDouble();
+            System.out.print("Digite o segundo valor a ser somado: ");
+            double secondNumber = input.nextDouble();
+        }
+        if (answer.contains("2")) {
+
+        }
+        if (answer.contains("3")) {
+
+        }
+        if (answer.contains("4")) {
+
+        }
+        if (answer.contains("5")) {
+
+        }
+        if (answer.contains("6")) {
+            answer = "";
+            mainMenu();
+        }
     }
 
     public void BMICalculatorMenu() {
@@ -38,13 +90,5 @@ public class Menus {
 
     public void AreaCalculatorMenu() {
         System.out.println("Calculadora de área");
-    }
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
     }
 }
