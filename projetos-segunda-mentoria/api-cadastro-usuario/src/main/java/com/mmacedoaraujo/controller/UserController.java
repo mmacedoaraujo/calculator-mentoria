ab974814d627444e6dbf8c28c8d9a3ec45d944ce
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-11-18 10:14:51 -0300

    populated database for test purposes

--- projetos-segunda-mentoria/api-cadastro-usuario/src/main/java/com/mmacedoaraujo/controller/UserController.java
+++ projetos-segunda-mentoria/api-cadastro-usuario/src/main/java/com/mmacedoaraujo/controller/UserController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
-
+    @GetMapping
     public ResponseEntity<List<User>> returnAllUsersRegistered() {
         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
