bb77486dcadae371fb688f5c971bba1bb58ce273
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-11-18 09:20:36 -0300

    created repository for user class

--- projetos-segunda-mentoria/api-cadastro-usuario/src/main/java/com/mmacedoaraujo/domain/User.java
+++ projetos-segunda-mentoria/api-cadastro-usuario/src/main/java/com/mmacedoaraujo/domain/User.java
@@ -4,6 +4,7 @@ import jdk.jfr.Timestamp;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "user_tb")
@@ -27,4 +28,16 @@ public class User {
     private Integer phoneNumber;
     private String email;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
